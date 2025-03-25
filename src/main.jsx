36efb5f6eea8f89b732e7c8105024bc81341d55b
unknown <sujoydaspc2023@gmail.com>
2025-03-25 23:49:08 +0600

    add firebase folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,17 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import React from 'react';
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Hello world!</div>,
+    },
+]);
 createRoot(document.getElementById('root')).render(
 
-    <App />
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 )

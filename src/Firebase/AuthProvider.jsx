dc822b6637219a36f151a5f901047a382ee2edd3
unknown <sujoydaspc2023@gmail.com>
2025-03-27 01:00:00 +0600

    add signout

--- src/Firebase/AuthProvider.jsx
+++ src/Firebase/AuthProvider.jsx
@@ -1,25 +1,78 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import { createContext } from "react";
+import auth from "./firebase.config";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const AuthContext = createContext()
 
 const AuthProvider = ({ children }) => {
     const [projectList, setProjects] = useState([])
-
+    const [user, setUser] = useState()
 
     useEffect(() => {
         fetch("/Projects.json")
             .then(res => res.json())
             .then(data => setProjects(data))
     }, [])
-    const data = {
-        projectList,
+
+
+    const SignUpUser = (email, password) => {
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((result) => {
+                setUser(result.user)
+                console.log(result.user);
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+    }
+
+    const logInUser = (email, password) => {
+        signInWithEmailAndPassword(auth, email, password)
+            .then((result) => {
+                setUser(result.user)
+                console.log(result.user);
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+    }
+
+    const signOutUser = () => {
+        signOut(auth)
+            .then(() => {
+                console.log("Sign Out Successfull");
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
     }
 
 
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            console.log(currentUser);
+        })
+        return () => {
+            unSubscribe();
+        }
+    }, [])
 
+
+
+
+
+
+    const data = {
+        user,
+        projectList,
+        logInUser,
+        SignUpUser,
+        signOutUser
+    }
     return (
         <AuthContext.Provider value={data}>
             {children}
